8b599f0c022937c43d4bd8c0916f6f037f4257a8
himanshusain0 <ht0257445@gmail.com>
2026-06-12 14:46:52 +0530

    PreparebaseModel

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from Kidney_disease import logger
 from Kidney_disease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+from Kidney_disease.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 STAGE_NAME = "Data Ingestion stage"
+
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -14,3 +17,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare base model"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
